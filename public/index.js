68eb29a5bc6276acfa7a54486fdfc4efae64e094
Mikel Duffy <mikelduffy1@gmail.com>
2016-12-08 11:41:20 -0800

    Creates client app structure

--- public/index.js
+++ public/index.js
@@ -74,3 +74,27 @@ app.app.authenticate()
     console.error(error);
   });
 
+
+// Sets up app to use authentication and socket.io
+const socket = io();
+const app = feathers()
+  .configure(feathers.socketio(socket))
+  .configure(feathers.hooks())
+  .configure(feathers.authentication({
+  storage: window.localStorage
+}));
+
+// React Router code here...
+app.authenticate().then(() => {
+  // If they are authenticated, route them to the right place...
+  render();
+}).catch(error => {
+  // Otherwise send them to the login page...
+  if (error.code === 401) {
+    // This assumes that we will have a login.html, but we can also implement this with react and use router...
+    window.location.href = '/login.html';
+  }
+  console.error(error);
+});
+
+export default app;
