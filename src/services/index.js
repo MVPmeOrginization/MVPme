2319a720f43e032b357822d1063b50b58bda5b52
Savaughn Jones <savaughnjones@gmail.com>
2016-12-07 22:15:19 -0800

    Add progress on database set-up.

--- src/services/index.js
+++ src/services/index.js
@@ -23,4 +23,9 @@ module.exports = function() {
   app.configure(user);
   app.configure(projects);
   app.configure(submissions);
+
+  Object.keys(sequelize.models)
+    .map(name => sequelize.models[name])
+    .filter(model => model.associate !== undefined)
+    .forEach(model => model.associate());
 };
--- src/services/submissions/submissions-model.js
+++ src/services/submissions/submissions-model.js
@@ -1,23 +1,41 @@
 'use strict';
 
 // submissions-model.js - A sequelize model
-// 
+//
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 
 const Sequelize = require('sequelize');
 
 module.exports = function(sequelize) {
-  const submissions = sequelize.define('submissions', {
-    text: {
+  const Submissions = sequelize.define('Submissions', {
+    title:{
       type: Sequelize.STRING,
       allowNull: false
+    },
+    description:{
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    videoLink: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    repoLink: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+    win:{
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
     }
+
   }, {
     freezeTableName: true
   });
 
-  submissions.sync();
+  Submissions.sync();
 
-  return submissions;
+  return Submissions;
 };
