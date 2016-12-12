67b36aa2e94d3391a753f76bd2f80f333722fea3
Mikel Duffy <mikelduffy1@gmail.com>
2016-12-12 12:40:06 -0800

    Fixes app.js

--- src/app.js
+++ src/app.js
@@ -32,33 +32,6 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
 
-
-
-app.use(webpackHotMiddleware(compiler, {
-  log: console.log,
-  path: '/__webpack_hmr',
-  heartbeat: 10 * 1000,
-}));
-const middleware = require('./middleware');
-const services = require('./services');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-
-const app = feathers();
-const compiler = webpack(webpackConfig);
-
-
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  hot: true,
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
-}));
-
-
-
 app.use(webpackHotMiddleware(compiler, {
   log: console.log,
   path: '/__webpack_hmr',
