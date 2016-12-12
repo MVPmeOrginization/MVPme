c725a515a5cc2df5b1b5b39ea5905f5a82d71d11
Mikel Duffy <mikelduffy1@gmail.com>
2016-12-12 15:17:34 -0800

    Cleans up project and submission forms

--- public/NewProjectFormView/NewProjectFormView.js
+++ public/NewProjectFormView/NewProjectFormView.js
@@ -1,19 +1,17 @@
 import {Grid, Row, Col, FormGroup, ControlLabel, FormControl, HelpBlock, Button, InputGroup, Jumbotron} from 'react-bootstrap';
 import React from 'react';
 
-
 const ProjectFormView = (props) => {
   return (
-    <div>
-    <Col xs={6} xs Offset={3}>
-      <Jumbotron>
-        <h3> Create A New Project </h3>
-      </Jumbotron>
-    </Col>
-
-      <Grid>
-        <Row>
-          <form onSubmit={props.postProject}>
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <h2 className="text-center"> Create A New Project </h2>
+        </Col>
+      </Row>
+      <Row>
+        <form onSubmit={props.postProject}>
+          <Row>
             <Col xs={6} xsOffset={3}>
             <FormGroup controlId = 'formControlsTitle' validationState = {props.getValidation('title')}>
               <ControlLabel>Project Title</ControlLabel>
@@ -22,7 +20,8 @@ const ProjectFormView = (props) => {
               <HelpBlock>Supply a title to the project.</HelpBlock>
             </FormGroup>
             </Col>
-
+          </Row>
+          <Row>
             <Col xs={6} xsOffset={3}>
             <FormGroup controlId = 'formControlsDescription' validationState = {props.getValidation('description')}>
               <ControlLabel>Project Vision and Description</ControlLabel>
@@ -31,7 +30,8 @@ const ProjectFormView = (props) => {
               <HelpBlock>Supply at least a base description of the vision for your project to give those competing for your bounty something to go off of.</HelpBlock>
             </FormGroup>
             </Col>
-
+          </Row>
+          <Row>
             <Col xs={6} xsOffset={3}>
             <FormGroup controlId = 'formControlsBounty' validationState = {props.getValidation('bounty')}>
               <ControlLabel>Bounty Amount</ControlLabel>
@@ -43,7 +43,8 @@ const ProjectFormView = (props) => {
               <HelpBlock>Supply a bounty to be awarded to the selected winner for your project. The bounty is the value of the contract to be awarded.</HelpBlock>
             </FormGroup>
             </Col>
-
+          </Row>
+          <Row>
             <Col xs={6} xsOffset={3}>
             <FormGroup controlId = 'formControlsEndDate' validationState = {props.getValidation('endDate')}>
               <ControlLabel>Project End Date</ControlLabel>
@@ -52,7 +53,8 @@ const ProjectFormView = (props) => {
               <HelpBlock>Please provide a cut off date for submission in mm/dd/yyyy format</HelpBlock>
             </FormGroup>
             </Col>
-
+          </Row>
+          <Row>
             <Col xs={6} xsOffset={3}>
             <FormGroup controlId = 'formControlsVideo'>
               <ControlLabel>Project Description Video (optional)</ControlLabel>
@@ -61,21 +63,23 @@ const ProjectFormView = (props) => {
               <HelpBlock>Please use embed url (example: https://www.youtube.com/embed/tntOCGkgt98)</HelpBlock>
             </FormGroup>
             </Col>
-
+          </Row>
+          <Row>
             <Col xs={6} xsOffset={3}>
-            <FormGroup controlId = 'formControlsPhoto'>  
+            <FormGroup controlId = 'formControlsPhoto'>
               <ControlLabel>Project Photo (optional)</ControlLabel>
               <FormControl type='file' onChange={props.updateState}  />
             </FormGroup>
             </Col>
-
-            <Col xs={12} xsOffset={5}>
+          </Row>
+          <Row>
+            <Col xs={6} xsOffset={3} className='text-center'>
             <Button type='submit'> Submit </Button>
             </Col>
-          </form>
-        </Row>
-      </Grid>
-    </div>
+          </Row>
+        </form>
+      </Row>
+    </Grid>
   )
 };
 
