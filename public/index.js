5d18c87bcbe0fb38fe21602db2f9d4be6db74e24
Mikel Duffy <mikelduffy1@gmail.com>
2016-12-12 19:52:20 -0800

    Renames folders

--- public/index.js
+++ public/index.js
@@ -3,12 +3,12 @@ import {Router, Route, browserHistory, Link, IndexRoute} from 'react-router';
 import {render} from 'react-dom';
 import app from './config.js';
 import Navigation from './navigation'
-import MvpForm from './mvpSubmissionView/index.js';
+import MvpForm from './mvpFormView/index.js';
 import LoginPage from './login/index.js';
 import SignupPage from './signup/index.js';
 import ProjectsView from './projectListView/index.js'
-import NonOwnerProjectView from './projectNonOwnerView/index.js';
-import ProjectForm from './NewProjectFormView/index.js';
+import NonOwnerProjectView from './projectView/index.js';
+import ProjectForm from './projectFormView/index.js';
 import ProjectOwnerView from './ProjectOwnerView/index.js';
 
 // import react router code...
